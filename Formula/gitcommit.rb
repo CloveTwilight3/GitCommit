c322e9961a4b30bd1d9e2daa5729611cf867010c
Clove Twilight <clovetwilight3@outlook.com>
2026-01-01 00:54:17 +0000

    Update Homebrew formula to version 1.1.3

--- Formula/gitcommit.rb
+++ Formula/gitcommit.rb
@@ -1,10 +1,10 @@
 class Gitcommit < Formula
   desc "Automated Git workflow tool"
   homepage "https://github.com/CloveTwilight3/GitCommit"
-  url "https://github.com/CloveTwilight3/GitCommit/archive/refs/tags/v1.1.0.tar.gz"
-  sha256 "" # This will be calculated during release
+  url "https://github.com/CloveTwilight3/GitCommit/archive/refs/tags/v1.1.3.tar.gz"
+  sha256 "05cab3b10a978c16942a15141aa95353ee1531082cfce8266b9ec6560dc231bc"
   license "Estrogen-Source-Available-1.2"
-  version "1.1.0"
+  version "1.1.3"
 
   depends_on "git"
   depends_on "bash"
